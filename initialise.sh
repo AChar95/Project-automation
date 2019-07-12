409e75720299d6359a2c0e2255c7fdaad698e11f
Allan Charles <50716770+AChar95@users.noreply.github.com>
2019-07-12 10:31:38 +0100

    Update initialise.sh

--- initialise.sh
+++ initialise.sh
@@ -3,6 +3,6 @@
 sudo apt -y update
 sudo apt -y upgrade
 sudo apt install -y mongodb
-sudo mv ./Project-automation/mongod.config /etc/
+sudo mv mongod.config /etc/
 sudo systemctl daemon-reload
 sudo systemctl start mongodb
