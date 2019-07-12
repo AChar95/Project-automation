4fd37b52c40f3a2320b3c7236f84977ab11a1d5a
AChar95 <allancharles95@outlook.com>
2019-07-12 10:33:41 +0100

    complete script for api

--- initialise.sh
+++ initialise.sh
@@ -2,28 +2,17 @@
 
 sudo apt -y update
 sudo apt -y upgrade
-sudo apt install -y mongodb
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt install npm
 git clone https://github.com/Nboaram/TeamAPoolProjectBackend.git
-git clone https://github.com/Nboaram/TeamAPoolProjectUI.git
 
 cd TeamAPoolProjectBackend
 git checkout Developer
-npm install
-cd ..
-cd TeamAPoolProjectUI
-git checkout Developer
 yes | npm install
-yes | sudo npm install -g @angular/cli
 cd ..
 
-
-sudo mv ./Project-automation/ui.service /etc/systemd/system/ui.service
 sudo mv ./Project-automation/api.service /etc/systemd/system/api.service
 
 sudo systemctl daemon-reload
-sudo systemctl start mongodb
-sudo systemctl start ui.service
 sudo systemctl start api.service
